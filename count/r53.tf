9b711d6f994c233b42bb26f92a9865a64ced81be
shaheer0071 <shaikshaheer007@gmail.com>
2025-02-08 14:53:46 +0530

    terraform practice

---
+++ count/r53.tf
@@ -0,0 +1,17 @@
+resource "aws_route53_record" "expense" {
+  count = 3
+  zone_id = var.zone_id
+  name    = "${var.intsances[count.index]}.${var.domain_name}"
+  type    = "A"
+  ttl     = 1
+  records = [aws_instance.expense[count.index].private_ip]
+}
+
+resource "aws_route53_record" "frontend" {
+#   count = 3
+  zone_id = var.zone_id
+  name    = "${var.domain_name}"
+  type    = "A"
+  ttl     = 1
+  records = [aws_instance.expense[2].public_ip]
+}
---
+++ count/variables.tf
@@ -0,0 +1,11 @@
+variable "intsances" {
+    default = ["mysql","backend","frontend"]
+  
+}
+variable "zone_id" {
+    default = "Z0303883E6ZLFMXCPUCZ"
+  
+}
+variable "domain_name" {
+    default = "mt15.store"
+}
