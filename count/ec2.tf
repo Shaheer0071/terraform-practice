324d8ad30d1d6b3098d7085bee7379f04f0ccf97
shaheer0071 <shaikshaheer007@gmail.com>
2025-02-08 15:46:24 +0530

    terraform practice

--- count/ec2.tf
+++ count/ec2.tf
@@ -4,11 +4,18 @@ resource "aws_instance" "expense" {
   ami                    = "ami-09c813fb71547fc4f" # This is our devops-practice AMI ID
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
   instance_type          = "t3.micro"
-  tags = {
-    Name    = var.intsances[count.index]
-    Purpose = "terraform-practice"
+  # tags = {
+  #   Name    = var.intsances[count.index]
+  #   Purpose = "terraform-practice"
+  # }
+  tags = merge(
+    var.coommon_tags,
+    {
+      Name = var.intsances[count.index]
+    }
+  )
   }
-}
+
 
 resource "aws_security_group" "allow_tls" {
   name        = "allow_tls1"
--- count/variables.tf
+++ count/variables.tf
@@ -9,3 +9,12 @@ variable "zone_id" {
 variable "domain_name" {
     default = "mt15.store"
 }
+variable "coommon_tags" {
+    type = map
+    default = {
+        project = "expense"
+        environment = "dev"
+        
+    }
+  
+}
